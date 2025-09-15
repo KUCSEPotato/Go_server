168cf1e1ba8963180059f17c550c5d56a922a53c
hansung-ku <hansungk@korea.ac.kr>
2025-09-15 11:33:38 +0900

    change CORS

--- locker-server/cmd/server/main.go
+++ locker-server/cmd/server/main.go
@@ -72,7 +72,7 @@ func main() {
 	// 전역 미들웨어 장착
 	app.Use(
 		cors.New(cors.Config{
-			AllowOrigins: "http://localhost:3000, https://kucisc.kr",
+			AllowOrigins: "https://www.kucisc.kr, https://kucisc.kr",
 			AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 			AllowMethods: "GET, POST, HEAD, PUT, DELETE, PATCH",
 		}),
--- locker-server/internal/db/migrate/005_truncate_user_data.sql
+++
@@ -1,5 +0,0 @@
--- This script truncates the users and auth_refresh_tokens tables to clear all user data.
--- It's used to reset user information, for example after changing the serial_id generation logic.
-
--- Truncate both tables, reset sequences, and handle foreign key constraints.
-TRUNCATE TABLE users, auth_refresh_tokens RESTART IDENTITY CASCADE;
